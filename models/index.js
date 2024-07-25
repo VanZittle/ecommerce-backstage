b7ae6c18c9110c9e96ffbfb27e278c24ab28a3af
VanZittle <vanzittle@gmail.com>
2024-07-25 02:46:28 -0400

    adding routes and models

--- models/index.js
+++ models/index.js
@@ -6,14 +6,20 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-
+  foreignKey: 'category_id',
 })
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+})
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag,{
 
+})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product,{
+  
+})
 module.exports = {
   Product,
   Category,
